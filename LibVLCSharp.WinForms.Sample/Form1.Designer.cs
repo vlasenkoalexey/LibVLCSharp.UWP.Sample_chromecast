469a82dc69af5edf090c3ec35ea95b15fc181b82
Aleksey Vlasenko <vlasenkoalexey@gmail.com>
2020-08-16 15:07:49 -0700

    - Added WinForms sample - Moved projects into separate folders under same solution

---
+++ LibVLCSharp.WinForms.Sample/Form1.Designer.cs
@@ -0,0 +1,103 @@
+﻿namespace LibVLCSharp.WinForms.Sample
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+                this._mp?.Dispose();
+                this._libVLC?.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.videoView1 = new LibVLCSharp.WinForms.VideoView();
+            this.button1 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.button3 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.videoView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // videoView1
+            // 
+            this.videoView1.BackColor = System.Drawing.Color.Black;
+            this.videoView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.videoView1.Location = new System.Drawing.Point(0, 0);
+            this.videoView1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.videoView1.MediaPlayer = null;
+            this.videoView1.Name = "videoView1";
+            this.videoView1.Size = new System.Drawing.Size(1200, 692);
+            this.videoView1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(0, 0);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(139, 34);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Open File";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(158, 4);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(182, 28);
+            this.comboBox1.TabIndex = 3;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(356, 2);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(103, 30);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "Cast";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1200, 692);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.videoView1);
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "Form1";
+            this.Text = "LibVLCSharp.WinForms";
+            ((System.ComponentModel.ISupportInitialize)(this.videoView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private VideoView videoView1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button button3;
+    }
+}
---
+++ LibVLCSharp.WinForms.Sample/Form1.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using LibVLCSharp.Shared;
+
+namespace LibVLCSharp.WinForms.Sample
+{
+    public partial class Form1 : Form
+    {
+        public LibVLC _libVLC;
+        public MediaPlayer _mp;
+        readonly HashSet<RendererItem> _rendererItems = new HashSet<RendererItem>();
+
+        public Form1()
+        {
+            if (!DesignMode)
+            {
+                Core.Initialize();
+            }
+
+            InitializeComponent();
+            _libVLC = new LibVLC();
+            _mp = new MediaPlayer(_libVLC);
+            _libVLC.Log += LibVLC_Log;
+            videoView1.MediaPlayer = _mp;
+            Load += Form1_Load;
+            _libVLC.SetDialogHandlers(OnDisplayError, OnDisplayLogin, OnDisplayQuestion, OnDisplayProgress, OnUpdateProgress);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            RendererDescription renderer;
+            renderer = _libVLC.RendererList.First();
+
+            // create a renderer discoverer
+            RendererDiscoverer _rendererDiscoverer = new RendererDiscoverer(_libVLC, renderer.Name);
+
+            // register callback when a new renderer is found
+            _rendererDiscoverer.ItemAdded += _rendererDiscoverer_ItemAdded; ;
+
+            // start discovery on the local network
+            bool result = _rendererDiscoverer.Start();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Invoke((Action)(() =>
+            {
+                using (OpenFileDialog openFileDialog = new OpenFileDialog())
+                {
+                    //openFileDialog.RestoreDirectory = true;
+                    openFileDialog.ShowHelp = true;
+
+                    if (openFileDialog.ShowDialog() == DialogResult.OK)
+                    {
+                        //Get the path of specified file
+                        _mp.Play(new Media(_libVLC, new Uri(openFileDialog.FileName)));
+                    }
+                }
+            }));
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            RendererDescription renderer;
+            renderer = _libVLC.RendererList.First();
+
+            // create a renderer discoverer
+            RendererDiscoverer _rendererDiscoverer = new RendererDiscoverer(_libVLC, renderer.Name);
+
+            // register callback when a new renderer is found
+            _rendererDiscoverer.ItemAdded += _rendererDiscoverer_ItemAdded; ;
+
+            // start discovery on the local network
+            bool result = _rendererDiscoverer.Start();
+
+        }
+
+        private void _rendererDiscoverer_ItemAdded(object sender, RendererDiscovererItemAddedEventArgs e)
+        {
+            Debug.WriteLine($"New item discovered: {e.RendererItem.Name} of type {e.RendererItem.Type}");
+            if (e.RendererItem.CanRenderVideo)
+                Debug.WriteLine("Can render video");
+            if (e.RendererItem.CanRenderAudio)
+                Debug.WriteLine("Can render audio");
+
+            this._rendererItems.Add(e.RendererItem);
+
+            Invoke((Action)(() =>
+            {
+                this.comboBox1.Items.Add(e.RendererItem.Name);
+            }));
+        }
+
+        private void LibVLC_Log(object sender, LogEventArgs e)
+        {
+            Debug.WriteLine($"LibVLC -> {e.Module}: {e.Message}");
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            videoView1.MediaPlayer.Stop();
+            videoView1.MediaPlayer.SetRenderer(_rendererItems.Where(rendererItem => rendererItem.Name == comboBox1.SelectedItem.ToString()).First());
+            videoView1.MediaPlayer.Play();
+        }
+
+        private async Task OnDisplayError(string title, string text)
+        {
+            Invoke((Action)(() =>
+            {
+                var result = MessageBox.Show(title, text,
+                                             MessageBoxButtons.OK,
+                                             MessageBoxIcon.Error);
+            }));
+        }
+
+        private async Task OnDisplayLogin(Dialog dialog, string title, string text, string defaultUsername, bool askStore, CancellationToken token)
+        {
+            Debugger.Launch();
+        }
+
+        private async Task OnDisplayQuestion(Dialog dialog, string title, string text, DialogQuestionType type, string cancelText, string firstActionText, string secondActionText, CancellationToken token)
+        {
+            Invoke((Action)(() =>
+            {
+                var result = MessageBox.Show(text, title,
+                                             MessageBoxButtons.YesNoCancel,
+                                             MessageBoxIcon.Error);
+                if (result == DialogResult.Yes)
+                {
+                    dialog.PostAction(1);
+                } 
+                else if (result == DialogResult.No)
+                {
+                    dialog.PostAction(2);
+                }
+                else if (result == DialogResult.Cancel)
+                {
+                    dialog.Dismiss();
+                }
+            }));
+        }
+
+        private async Task OnDisplayProgress(Dialog dialog, string title, string text, bool indeterminate, float position, string cancelText, CancellationToken token)
+        {
+            Debugger.Launch();
+        }
+
+        private async Task OnUpdateProgress(Dialog dialog, float position, string text)
+        {
+            Debugger.Launch();
+        }
+    }
+}
